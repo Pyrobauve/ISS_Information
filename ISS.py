32b0e83e55f1eca346893574ed981b7b7a96a475
Pyrobauve <48654473+Pyrobauve@users.noreply.github.com>
2021-04-06 01:09:32 +0200

    Update ISS.py

--- ISS.py
+++ ISS.py
@@ -1,48 +1,78 @@
+import discord
+from discord.ext import commands , tasks
 import json
 import urllib.request
 from geopy.geocoders import Nominatim
+import config
 
-geolocator = Nominatim(user_agent="ISS_information")
-url_people = "http://api.open-notify.org/astros.json"
-url_pos = "http://api.open-notify.org/iss-now.json"
-data_people = urllib.request.urlopen(url_people)
-data_pos = urllib.request.urlopen(url_pos)
-resultat_people = json.loads(data_people.read())
-resultat_pos = json.loads(data_pos.read())
+TOKEN = config.TOKEN
 
-number_people = resultat_people['number']
-people_in = resultat_people['people']
+bot = commands.Bot(command_prefix='/')
+bot.remove_command('help')
 
-pos_ISS = resultat_pos['iss_position']
-lat = str(pos_ISS['latitude'])
-lon = str(pos_ISS['longitude'])
-location = geolocator.reverse(lat+","+lon)
-if location != None:
-    address = location.raw['address']
-    city = address.get('city', '')
-    state = address.get('state', '')
-    country = address.get('country', '')
+@bot.event
+async def on_ready():
+    print('---------------')
+    print(bot.user.name)
+    print('---------------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the space"))
 
-print("------- In the ISS -------")
-print("Number of people in the ISS:", number_people)
-print("")
-for p in people_in:
-    print(p['name'])
+@bot.command()
+async def info(ctx):
+    async with ctx.typing():
+        geolocator = Nominatim(user_agent="ISS_information")
+        url_people = "http://api.open-notify.org/astros.json"
+        url_pos = "http://api.open-notify.org/iss-now.json"
+        data_people = urllib.request.urlopen(url_people)
+        data_pos = urllib.request.urlopen(url_pos)
+        resultat_people = json.loads(data_people.read())
+        resultat_pos = json.loads(data_pos.read())
 
-print('')
-print('------- Location -------')
+        number_people = resultat_people['number']
+        people_in = resultat_people['people']
 
-if location != None:
-    if city != "":
-        print("City: ", city)
-    if state != "":
-        print("State: ", state)
-    if country != "":
-        print("Country: ", country)
+        pos_ISS = resultat_pos['iss_position']
+        lat = str(pos_ISS['latitude'])
+        lon = str(pos_ISS['longitude'])
+        try:
+            location = geolocator.reverse(f"{lat},{lon}")
+            if location != None:
+                address = location.raw['address']
+                city = address.get('city', '')
+                state = address.get('state', '')
+                country = address.get('country', '')
+        except:
+            location = "error"
+        f = open("data.txt", "w")
+        f.write("")
+        f.close()
 
-else:
-    print("The ISS is above the ocean.")
+        f = open("data.txt", "a")
 
-print("")
-print(f"The coordinate is: {lat}, {lon}")
-print(f"Url: https://www.openstreetmap.org/search?query={lat}%2C%20{lon}#map=5/{lat}/{lon}")
+        f.write("------- In the ISS -------\n")
+        f.write(f"Number of people in the ISS: {number_people} \n\n")
+        for p in people_in:
+            f.write(f"{p['name']}\n")
+
+        f.write('\n------- Location -------\n')
+
+        if location != None:
+            if city != "":
+                f.write(f"City: {city}\n")
+            if state != "":
+                f.write(f"State: {state}\n")
+            if country != "":
+                f.write(f"Country: {country}\n")
+        elif location == "error":
+            f.write("|| GeoPy currently has a problem. ||")
+        else:
+            f.write("The ISS is above the ocean.\n")
+
+        f.write(f"\nThe coordinate is: {lat}, {lon}\n")
+        f.write(f"Url: https://www.latlong.net/c/?lat={lat}&long={lon}")
+        f.close()
+
+        f = open("data.txt", "r")
+    await ctx.send(f.read())
+
+bot.run(TOKEN)
